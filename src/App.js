36161e3c0571ceba6733da0614b0a7416a1e8e43
Don Daniel <dondaniel028@gmail.com>
2024-07-01 08:53:29 +0100

    Initial push

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const blobs = ['blob-1', 'blob-2', 'blob-3', 'blob-4', 'blob-5', 'blob-6', 'blob-7', 'blob-8', 'blob-9', 'blob-10']
+  const [active, setActive] = useState(0)
+  function changeBlob() {
+    if(blobs[active+1])
+      return setActive(active+1)
+    return setActive(0)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header flex flex-col space-y-36">
+        {/* Insert Blob Here */}
+        <div className={`${blobs[active]} transition-all duration-300`}></div>
+
+        <button onClick={changeBlob} className='bg-yellow-800 px-5 py-3 rounded text-sm'>
+          <code>Change Blob</code>
+        </button>
+
       </header>
     </div>
   );
